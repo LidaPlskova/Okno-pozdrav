778db03574ba6686c4aff6462edda6557570013d
liduska <ludlidla@gmail.com>
2016-12-08 22:49:37 +0100

    zformátované

--- src/cz/plskova/HlavniOkno.java
+++ src/cz/plskova/HlavniOkno.java
@@ -5,55 +5,55 @@ import javax.swing.*;
 
 public class HlavniOkno extends JFrame {
 
-        private JLabel labJmeno;
-        private JTextField txtVstup;
-        private JButton btnPozdrav;
-        private JLabel labVystup;
+    private JLabel labJmeno;
+    private JTextField txtVstup;
+    private JButton btnPozdrav;
+    private JLabel labVystup;
 
-        public HlavniOkno (){
+    public HlavniOkno() {
 
-               this.setSize(350,400);
-               this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-               this.setLayout(null);
+        this.setSize(350, 400);
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setLayout(null);
 
-                labJmeno = new JLabel();
-               labJmeno.setSize(100,20);
-               labJmeno.setLocation(20, 20);
-               labJmeno.setText("Zadej jméno");
+        labJmeno = new JLabel();
+        labJmeno.setSize(100, 20);
+        labJmeno.setLocation(20, 20);
+        labJmeno.setText("Zadej jméno");
 
-                txtVstup =  new JTextField();
-               txtVstup.setSize(150, 20);
-               txtVstup.setLocation(100, 20);
+        txtVstup = new JTextField();
+        txtVstup.setSize(150, 20);
+        txtVstup.setLocation(100, 20);
 
-                btnPozdrav = new JButton();
-               btnPozdrav.setSize(100,20);
-               btnPozdrav.setLocation(50, 60);
-               btnPozdrav.setText("Pozdrav");
-               btnPozdrav.addMouseListener(new MouseAdapter() {
-                       @Override
-                       public void mouseClicked(MouseEvent e) {
-                               poKliknutiNaTlacitko(e);
-                       }
-               });
+        btnPozdrav = new JButton();
+        btnPozdrav.setSize(100, 20);
+        btnPozdrav.setLocation(50, 60);
+        btnPozdrav.setText("Pozdrav");
+        btnPozdrav.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                poKliknutiNaTlacitko(e);
+            }
+        });
 
-                labVystup = new JLabel();
-               labVystup.setSize(300, 40);
-               labVystup.setLocation(20, 80);
+        labVystup = new JLabel();
+        labVystup.setSize(300, 40);
+        labVystup.setLocation(20, 80);
 
+        //pridame do okna objekty, aby se objevily
 
-               //pridame do okna objekty, aby se objevily
+        this.add(labJmeno);
+        this.add(txtVstup);
+        this.add(btnPozdrav);
+        this.add(labVystup);
 
-               this.add(labJmeno);
-               this.add(txtVstup);
-               this.add(btnPozdrav);
-               this.add(labVystup);
+        //aby se vše vykreslilo
+        this.setVisible(true);
 
-               //aby se vše vykreslilo
-               this.setVisible(true);
+    }
 
-           }
-        private void poKliknutiNaTlacitko (MouseEvent e){
-             labVystup.setText("Jé, "+ txtVstup.getText()+"! Ahoj!");
-        }
-       }
+    private void poKliknutiNaTlacitko(MouseEvent e) {
+        labVystup.setText("Jé, " + txtVstup.getText() + "! Ahoj!");
+    }
+}
 
